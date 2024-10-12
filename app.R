622773938cb46e0144a16878958f504ed28050c6
henrytran <126839412+nguyenhaitran@users.noreply.github.com>
2024-10-12 22:03:06 +0800

    Finish the Shiny app in app.R

--- app.R
+++ app.R
@@ -1,11 +1,206 @@
-```{r}
+# Install all the required packages for data processing, data transformation and data visualization
+library(ggplot2)
+library(shiny)
+library(gridExtra)
+library(reshape2)
+library(readxl)
+library(dplyr)
 
+#insert the data
+path<-url("https://lms.uwa.edu.au/bbcswebdav/pid-3998436-dt-content-rid-47695412_1/courses/CITS4009_SEM-2_2024/Countries%20and%20death%20causes.csv")
+data$Entity<-as.factor(data$Entity)
+data$Code<-as.factor(data$Code)
+data$Year<-as.factor(data$Year)
+
+region_mask <- grepl("(WB)|(WHO)|^World|^G20|^OECD", data$Entity)
+regions<-data[region_mask,]
+countries<-data[!region_mask,]
+
+#here is my file path, please change to your filepath to where you downloaded the file to run the code below
+path<-"/Users/henrytran/Documents/UWA_Master-Data-Science/CITS4009/Project/"
+
+#create a path that can access and extractn the data
+country_groups_file<- paste0(path, "World Bank Country Groups.xlsx")
+country_groups_dataset<-read_excel(country_groups_file, sheet = "List of economies")
+country_groups_dataset<-country_groups_dataset[,c(1,2,4)]
+
+countries_merged<-merge(countries, country_groups_dataset, by.x="Code", by.y="Code", all.x=TRUE)
+#we keep the remaining countries
+countries_merged<-merge(countries, country_groups_dataset, by.x="Code", by.y="Code")
+
+#for the data, group the data into entity and income groups, then calculate the total death of each death causes of each country
+names(countries_merged)[names(countries_merged) == "Income group"] <- "Income.group"
+
+total_countries_death <- countries_merged %>% group_by(Entity, Income.group) %>% summarise(across(c("Outdoor.air.pollution": "Iron.deficiency"), sum))
+
+label_total_countries_death<-total_countries_death
+label_total_countries_death$Label<-ifelse(total_countries_death$Income.group == "High income", 1, 0)
+label_total_countries_death$Label<-as.factor(label_total_countries_death$Label)
+
+set.seed(12345)
+random_mask<-runif(nrow(label_total_countries_death))<0.8 #80% of data will be used for training, 20% of data will be used for testing
+train_data<-label_total_countries_death[random_mask,]
+test_data<-label_total_countries_death[!random_mask,]
+
+train_data<-train_data[,3:ncol(train_data)]
+test_data<-test_data[,3:ncol(test_data)]
+
+responses<- colnames(label_total_countries_death) %in% c("Entity", "Income.group", "Label") # create a mask to filter out the responses and the features 
+all_features<-colnames(label_total_countries_death)[!responses]
+
+mkPredC<-function(outCol, varCol, appCol){ #training outcomes, training variable and prediction variable
+  #we convert all the data to vector for easier calculation
+  outCol <- as.vector(outCol) 
+  varCol <- as.vector(varCol)
+  appCol <- as.vector(appCol)
+  pOne<-sum(outCol==1)/length(outCol) #the probability of the outcome is 1 during training
+  vTab<-table(outCol, varCol) #the table of training outcomes and training variable
+  pOneWv<-(vTab[1,]+ 1.0e-3 *pOne)/(colSums(vTab)+ 1.0e-3) #how often the outcome is 1 with conditioned on levels of training variable
+  pred<-pOneWv[appCol] #make prediction by looking up levels of appCol
+  pred[is.na(pred)]<-pOne #add prediction for appCol that were unknown when training
+  pred
+}
+
+mkPredN<- function(outCol, varCol, appCol){
+  varCol <- as.numeric(varCol)  #we convert all the data to numerical to use for quantile function
+  appCol <- as.numeric(appCol)  #we convert all the data to numerical to use for quantile function
+  cuts<-unique(quantile(varCol, probs = seq(0,1,0.1), na.rm=T)) #we convert the numerical data to groups of categorical data
+  varC<-cut(varCol, cuts) #we applied the cuts to the training variables
+  appC<-cut(appCol, cuts) #we applied the cuts to the testing variable
+  mkPredC(outCol, varC, appC)
+}
+
+#this is the code followed from the lecture, with some modification
+#import the ROCR library to create calcAUC function used for calculating single-variate model's AUCs 
+library('ROCR')
+calcAUC <- function(predcol,outcol) {
+  perf <- performance(prediction(predcol,outcol==1),'auc') #we are checking the performance of model predicting the value 1
+  as.numeric(perf@y.values)
+}
+
+#this is the code followed from the lecture, with some modification
+performanceMeasures <- function(pred, truth, name = "model") {
+  # Construct confusion matrix ensuring all levels (0 and 1) are present
+  ctable <- table(factor(truth, levels = c(0, 1)), factor(pred > 0.5, levels = c(FALSE, TRUE)))
+  accuracy <- sum(diag(ctable)) / sum(ctable) #(TP+TN)/(TP+FP+TN+FN)
+  precision <- ifelse(sum(ctable[, 2]) == 0, 0, ctable[2, 2] / sum(ctable[, 2])) #we prevent the division with 0, (TP)/(TP+FP)
+  recall <- ifelse(sum(ctable[2, ]) == 0, 0, ctable[2, 2] / sum(ctable[2, ])) #we prevent the division with 0, (TP)/(TP+FN)
+  f1 <- ifelse(precision + recall == 0, 0, 2 * precision * recall / (precision + recall)) #we prevent the division with 0, (2*precision*recall)/(precision+recall)
+  data.frame(model = name, precision = precision, recall = recall, f1 = f1, accuracy = accuracy)
+}
+
+pretty_perf_table <- function(model,training,test, feature, model_type) {
+  library(pander)
+  # setting up Pander Options
+  panderOptions("plain.ascii", TRUE)
+  panderOptions("keep.trailing.zeros", TRUE)
+  panderOptions("table.style", "simple")
+  perf_justify <- "lrrrr"
+  # comparing performance on training vs. test
+  if (model_type == "logistic") {
+    pred_train <- predict(model, newdata=training[, feature], type="response") #for logistic regression, we need to set predict type to "response"
+    pred_test <- predict(model, newdata=test[, feature], type="response")
+  } else if (model_type == "decision_tree") {
+    pred_train <- predict(model, newdata=training[, feature], type="prob")[, 2]  # for logistic regression, we need to set predict type to "prob", and take the second column, as the result
+    pred_test <- predict(model, newdata=test[, feature], type="prob")[, 2]
+  }
+  truth_train <- training[["Label"]]
+  truth_test <- test[["Label"]]
+  
+  trainperf_tree <- performanceMeasures(pred_train, truth_train, "logistic, training")
+  testperf_tree <- performanceMeasures(pred_test, truth_test, "logistic, test")
+  
+  perftable <- rbind(trainperf_tree, testperf_tree)
+  pandoc.table(perftable, justify = perf_justify)
+}
+
+library(rpart)
+library(rpart.plot)
+
+#this is the code followed from the lecture, with some modification
+library(ROCit)
+# Function to plot ROC curves for both logistic regression and decision tree models
+plot_roc <- function(predcol1_test, outcol1_test, predcol1_train, outcol1_train, 
+                     predcol2_test, outcol2_test, predcol2_train, outcol2_train) {
+  #we calcualte the ROC for the first model, which is the logistic regression model
+  roc_1_test <- rocit(score = predcol1_test, class = outcol1_test == 1)
+  roc_1_train <- rocit(score = predcol1_train, class = outcol1_train == 1)
+  #we calcualte the ROC for the first model, which is the decision tree model
+  roc_2_test <- rocit(score = predcol2_test, class = outcol2_test == 1)
+  roc_2_train <- rocit(score = predcol2_train, class = outcol2_train == 1)
+  # Plot test data for both models
+  plot(roc_1_test, col = c("blue", "green"), lwd = 2, legend = FALSE, YIndex = FALSE, values = TRUE, asp = 1)
+  lines(roc_2_test$TPR ~ roc_2_test$FPR, col = c("red", "green"), lwd = 2)
+  lines(roc_1_train$TPR ~ roc_1_train$FPR, col = c("blue", "green"), lwd = 2, lty = 2)
+  lines(roc_2_train$TPR ~ roc_2_train$FPR, col = c("red","green"), lwd = 2, lty = 2)
+  #we add the legend to classify the training and testing lines for each model
+  legend("bottomright", col = c("blue", "red","green"), 
+         legend = c("Logistic Test", "Decision Tree Test", "Null Model", "Logistic Train", "Decision Tree Train"),
+         lwd = 2,lty = c(1, 1, 2, 2, 2))
+}
+
+total_regions_death <- regions %>% group_by(Entity) %>% summarise(across(c("Outdoor.air.pollution": "Iron.deficiency"), sum))
+total_regions_death
+cluster_data<-total_regions_death
+
+scale_data<-scale(cluster_data[, 2:ncol(cluster_data)]) # this will be the data where each column has 0 mean and unit standard deviation
+
+library('grDevices')
+#this function will return the vector of points in each convex hull
+find_convex_hull <- function(proj2Ddf, groups) {
+  do.call(rbind,lapply(unique(groups), FUN = function(c) {
+    f <- subset(proj2Ddf, cluster==c);
+    f[chull(f),] # this compute the convex hull of a set of points
+  }))
+}
+
+#these are the code followed from the lecture, with some modification.
+#this function will return the squared Euclidean distance of two given points x and y from the cluster centroid
+sqr_euDist <- function(x, y) {
+  sum((x - y)^2)
+}
+#this function calculates WSS of a cluster, represented as a n-by-d matrix (n is number of rows and d is the number of column) which contains only points of the cluster.
+wss <- function(clustermat) {
+  c0 <- colMeans(clustermat)
+  sum(apply( clustermat, 1, FUN=function(row) {sqr_euDist(row, c0)} ))
+}
+#this function will calculate the total WSS.
+wss_total <- function(scaled_df, labels) {
+  wss.sum <- 0
+  k <- length(unique(labels))
+  for (i in 1:k)
+    wss.sum <- wss.sum + wss(subset(scaled_df, labels == i))
+  wss.sum
+}
+#this function will calculate the total sum of squared (TSS) distance of data points about the mean. This is the same as WSS when the k equals to 1.
+tss <- function(scaled_df) {
+  wss(scaled_df)
+}
+
+calculate_indices <- function(scaled_df, kmax) {
+  npts <- nrow(scaled_df)
+  wss.value <- numeric(kmax) # create a vector of numeric type
+  # wss.value[1] stores the WSS value for k=1 (when all the
+  # data points form 1 large cluster).
+  wss.value[1] <- wss(scaled_df)
+  d <- dist(scaled_df, method="euclidean")
+  pfit <- hclust(d, method="ward.D2")
+  for (k in 2:kmax) {
+    labels <- cutree(pfit, k=k)
+    wss.value[k] <- wss_total(scaled_df, labels)
+  }
+  bss.value <- tss(scaled_df) - wss.value # this is a vector
+  B <- bss.value / (0:(kmax-1)) # also a vector
+  W <- wss.value / (npts - 1:kmax) # also a vector
+  data.frame(k = 1:kmax, CH_index = B/W, WSS = wss.value)
+}
+library(gridExtra)
 
 logistic_model <- NULL
 decision_model <- NULL
 # Define UI for application that draws different plot
 ui <- navbarPage(
-  titlePanel("Visualisation of death in country/region during 1990 - 2019"),
+  "Analysis of number of recorded deaths and causes in countries/regions during 1990 - 2019",
   tabPanel("EDA",
            sidebarLayout(
              sidebarPanel(
@@ -236,4 +431,3 @@ server <- function(input, output){
 
 # Run the Shiny app
 shinyApp(ui = ui, server = server)
-```
